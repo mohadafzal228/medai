0326ea466e975c079055e0d4fa7a357d7c52e473
Muhammad Ali Ramzan <aliramzan0009@gmail.com>
2025-11-27 23:21:26 +0500

    Fix

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -32,7 +32,7 @@ const Register = () => {
             setLoading(false);
         }
     };
-
+    // chnnnnnxaknakjnaknsnanakjn
     return (
         <div className="min-h-screen flex items-center justify-center bg-slate-50 px-4 relative overflow-hidden">
             {/* Background Image */}
